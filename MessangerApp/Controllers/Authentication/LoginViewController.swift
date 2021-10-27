42da2fc6a3c42170c55cf0485844674208c690fd
administrator <fgaidhus@gmail.com>
2021-10-27 17:37:03 +0100

    section 2 registerAndLogin views

--- MessangerApp/Controllers/Authentication/LoginViewController.swift
+++ MessangerApp/Controllers/Authentication/LoginViewController.swift
@@ -11,9 +11,25 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setbackground()
     }
 
-
+    @IBAction func ToRegister(_ sender: Any) {
+        let addvc = UIStoryboard.init(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "register") as? RegisterViewController
+               self.navigationController?.pushViewController(addvc!, animated: true)
+        
+        print("hereee")
+        
+    }
+    
+    func setbackground () {
+                //background
+                let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+                backgroundImage.image = UIImage(named: "chatt.png")
+                backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+                self.view.insertSubview(backgroundImage, at: 0)
+        
+    }
 }
 
