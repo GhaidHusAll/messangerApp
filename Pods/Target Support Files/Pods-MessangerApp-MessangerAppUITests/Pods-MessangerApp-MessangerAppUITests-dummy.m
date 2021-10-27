8fb27688806951b72ecba401ba0fe36e38159e75
administrator <fgaidhus@gmail.com>
2021-10-27 12:37:25 +0100

    commit section 1

---
+++ Pods/Target Support Files/Pods-MessangerApp-MessangerAppUITests/Pods-MessangerApp-MessangerAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MessangerApp_MessangerAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MessangerApp_MessangerAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-MessangerAppTests/Pods-MessangerAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MessangerAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_MessangerAppTests
+@end
